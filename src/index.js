d3e78dd4cade24a8f4bf39385114c1b54475e627
Mustafizur <sayhemrahman@gmail.com>
2022-01-02 17:28:34 +0600

    added agent middlewares actiontypes indexjs store working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, history } from './store';
+import { ConnectedRouter } from 'react-router-redux';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </ConnectedRouter>
+    </Provider>
+
+  ),
   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function

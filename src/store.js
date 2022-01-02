167cb2e6f99d6e40b927222660f25981259ea467
Mustafizur <sayhemrahman@gmail.com>
2022-01-02 17:35:28 +0600

    added store reducer and reducers article

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { promiseMiddleware, localStorageMiddleware } from './middleware';
+import reducer from './reducer';
+
+
+
+
+export const history = createHistory();
+const myRouterMiddleware = routerMiddleware(history);
+
+
+const getMiddleware = () => {
+    if (process.env.NODE_ENV === 'production') {
+        return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware);
+    } else {
+        // Enable additional logging in non-production environments.
+        return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware, createLogger())
+    }
+};
+
+export const store = createStore(
+    reducer, composeWithDevTools(getMiddleware()));

d7bb1d5c59a9273e01a34d4f1811bdb6d22c8fc1
Hasan Nomaan <hasannoman31@gmail.com>
2022-01-04 05:06:09 +0600

    added

---
+++ Team-61-Projects-react-redux/src/components/Home/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Banner = ({ appName, token }) => {
+    if (token) {
+        return null;
+    }
+    return (
+        <div className="banner">
+            <div className="container">
+                <h1 className="logo-font">
+                    {appName.toLowerCase()}
+                </h1>
+                <p>Team Task to Share our Experience and Knowledge</p>
+
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
